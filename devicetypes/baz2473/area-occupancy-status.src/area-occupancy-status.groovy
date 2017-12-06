f1dd36f9c6cd8432e37021637de756bbd955b207
Barry Crook <Baz2473@icloud.com>
2017-12-06 03:12:48 +0000

    Off On Together

--- devicetypes/baz2473/area-occupancy-status.src/area-occupancy-status.groovy
+++ devicetypes/baz2473/area-occupancy-status.src/area-occupancy-status.groovy
@@ -1,47 +1,13 @@
-/*****************************************************************************************************************
-*
-*  A SmartThings device handler for handling Areas as devices which have states.
-*  Copyright (C) 2017 Baz2473
-*  Copyright (C) 2017 bangali
-*
-*  License:
-*  This program is free software: you can redistribute it and/or modify it under the terms of the GNU 
-*  General Public License as published by the Free Software Foundation, either version 3 of the License, or 
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
-*  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-*  for more details.
-*
-*  You should have received a copy of the GNU General Public License along with this program.
-*  If not, see <http://www.gnu.org/licenses/>.
-*
-*  Name: Area Occupancy Status
-*
-*  Version: 3.0.0
-*  Added Automation Status Custom Commands So They Are Available To WebCoRE.
-*
-*  Version: 2.1.1
-*  Minor Changes And Updates To Away Modes And Automation Events & Logs.
-*
-*  Version: 2.1.0
-*  Changed Display & Added A New Tile To Display The Automation Status Seperatley To The Occupancy Status.
-*
-*  Version: 1.0.4
-*  Removed NON CLICKABLE Buttons  
-*
-*  Version: 1.0.3
-*  Removed Click Options On Buttons That Should Not Be Clickable, The Purpose In My Occupancy App Is To Automatically Detect Status.
-*  I Have Left 'VACANT' And 'OUT OF ORDER' As Clickable In Case It Fails To Detect Vacancy,
-*  And To Override All Automation With The 'OUT OF ORDER' (OOO) Button.
-*
-*  Version: 1.0.2
-*  Updated OOO Icon And Color On All Mini Tiles For Better Viewing
-*
-*  Version: 1.0.1
-*  Updated to bring Versions in line  
-*
-*****************************************************************************************************************/
+/*
+  A SmartThings device handler for handling Areas as devices which have states.
+  
+  Copyright (C) 2017 Baz2473
+  
+  Name: Area Occupancy Status
+
+  Version: 1.0.0  
+
+*/
 
 metadata {
 	definition (
@@ -53,11 +19,17 @@ metadata {
 		attribute "occupancyStatus", "string"
         attribute "automationStatus", "string"
 		command "vacant"
+        command "vacanton"
         command "occupied"
+        command "occupiedon"
         command "checking"
+        command "checkingon"
 		command "engaged"
+        command "engagedon"
 		command "donotdisturb"
+        command "donotdisturbon"
 		command "heavyuse"
+        command "heavyuseon"
         command "automationon"
         command "automationoff"
         command "updateOccupancyStatus", ["string"]
@@ -70,12 +42,18 @@ metadata {
 	tiles(scale: 2)		{
     	multiAttributeTile(name: "occupancyStatus", width: 2, height: 2, canChangeBackground: false)		{
 			tileAttribute ("device.occupancyStatus", key: "PRIMARY_CONTROL")		{
-				attributeState "vacant", label: 'Vacant', icon:"st.Home.home18", backgroundColor:"#ffffff"
-                attributeState "occupied", label: 'Occupied', action: "vacant", icon:"st.Home.home4", backgroundColor:"#32cd32"
-                attributeState "checking", label: 'Checking', action: "vacant", icon:"st.Health & Wellness.health9", backgroundColor:"#e86d13"
-				attributeState "engaged", label: 'Engaged', action: "vacant", icon:"st.locks.lock.locked", backgroundColor:"#ff0000"
-                attributeState "donotdisturb", label: 'Do Not Disturb', action: "vacant", icon:"st.Office.office6", backgroundColor:"#00abf5"
-				attributeState "heavyuse", label: 'Heavy Use', action: "vacant", icon:"st.Health & Wellness.health5", backgroundColor:"#8a5128"
+				attributeState "vacant", label: 'OFF', icon:"st.Home.home18", backgroundColor:"#ffffff"
+                attributeState "vacanton", label: 'ON', icon:"st.Home.home18", backgroundColor:"#ffffff"
+                attributeState "occupied", label: 'OFF', action: "vacant", icon:"st.Home.home4", backgroundColor:"#32cd32"
+                attributeState "occupiedon", label: 'ON', action: "vacant", icon:"st.Home.home4", backgroundColor:"#32cd32"
+                attributeState "checking", label: 'OFF', action: "vacant", icon:"st.Health & Wellness.health9", backgroundColor:"#e86d13"
+                attributeState "checkingon", label: 'ON', action: "vacant", icon:"st.Health & Wellness.health9", backgroundColor:"#e86d13"
+				attributeState "engaged", label: 'OFF', action: "vacant", icon:"st.locks.lock.locked", backgroundColor:"#ff0000"
+                attributeState "engagedon", label: 'ON', action: "vacant", icon:"st.locks.lock.locked", backgroundColor:"#ff0000"
+                attributeState "donotdisturb", label: 'OFF', action: "vacant", icon:"st.Office.office6", backgroundColor:"#00abf5"
+                attributeState "donotdisturbon", label: 'ON', action: "vacant", icon:"st.Office.office6", backgroundColor:"#00abf5"
+				attributeState "heavyuse", label: 'OFF', action: "vacant", icon:"st.Health & Wellness.health5", backgroundColor:"#8a5128"
+                attributeState "heavyuseon", label: 'ON', action: "vacant", icon:"st.Health & Wellness.health5", backgroundColor:"#8a5128"
             }
        		tileAttribute ("device.status", key: "SECONDARY_CONTROL")	{
 				attributeState "default", label:'${currentValue}'
@@ -93,6 +71,7 @@ metadata {
 		main (["occupancyStatus"])
 		details (["occupancyStatus","automationStatus"])
 	}
+  preferences {}            	 
 }
 
 def parse(String description)	{}
@@ -104,16 +83,22 @@ def updated()	{   initialize()    }
 def initialize() {}
 
 def vacant()	{	stateUpdate('vacant')		}
+def vacanton()	{	stateUpdate('vacanton')		}
 
 def occupied()	{	stateUpdate('occupied')		}
+def occupiedon()	{	stateUpdate('occupiedon')		}
 
 def checking()	{	stateUpdate('checking')		}
+def checkingon()	{	stateUpdate('checkingon')		}
 
 def engaged()	{	stateUpdate('engaged')		}
+def engagedon()	{	stateUpdate('engagedon')		}
 
 def donotdisturb()	{	stateUpdate('donotdisturb')		}
+def donotdisturbon()	{	stateUpdate('donotdisturbon')		}
 
 def heavyuse()		{	stateUpdate('heavyuse')		}
+def heavyuseon()		{	stateUpdate('heavyuseon')		}
 
 def automationon() {  automationStateUpdate('automationon')  }
 
@@ -131,9 +116,9 @@ private	automationStateUpdate(automationState)	{
 }
 private updateOccupancyStatus(occupancyStatus = null) 	{
 	occupancyStatus = occupancyStatus?.toLowerCase()
-	def msgTextMap = ['vacant':'Vacant Since: ', 'occupied':'Occupied Since: ', 'checking':'Checking Status: ', 'engaged':'Engaged Since: ', 'donotdisturb':'Do Not Disturb!: ', 'heavyuse':'Area Is In Constant Use: ', 'outoforder':'Area Has Been OUT OF ORDER since: ']
+	def msgTextMap = ['vacant':'Vacant Since: ', 'vacanton':'Vacanton Since: ', 'occupied':'Occupied Since: ', 'occupiedon':'Occupiedon Since: ','checking':'Checking Status: ','checkingon':'Checkingon Status: ','engaged':'Engaged Since: ','engagedon':'Engagedon Since: ' ,'donotdisturb':'Do Not Disturb!: ','donotdisturbon':'Do Not Disturbon!: ', 'heavyuse':'Area Is In Constant Use: ','heavyuseon':'Area Is on In Constant Use: ']
 	if (!occupancyStatus || !(msgTextMap.containsKey(occupancyStatus))) {
-    	log.debug "${device.displayName}: Missing or invalid parameter Occupancy Status. Allowed values are: Vacant, Occupied, Checking, Engaged, Heavyuse, Donotdisturb or Outoforder."
+    	log.debug "${device.displayName}: Missing or invalid parameter Occupancy Status. Allowed values are: Vacant, Occupied, Checking, Engaged, Heavyuse, Donotdisturb, Vacanton, Occupiedon, Checkingon, Engagedon, Heavyuseon or Donotdisturbon."
         return
     }
 	sendEvent(name: "occupancyStatus", value: occupancyStatus, descriptionText: "${device.displayName} changed to ${occupancyStatus}", isStateChange: true, displayed: true)
